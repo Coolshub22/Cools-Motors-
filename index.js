f63b895ab6d41690b31bcdc8c0df3b8e0af4156b
brian23ka <brianngenye@gmail.com>
2025-03-26 16:19:09 +0300

    function display cars in the gallery

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ function getCars() {
     })
     .catch(error => console.error("Error fetching cars:", error));
 }
+
+function showGallery(cars) {
+    let gallery = document.getElementById("car-list");
+    gallery.innerText ="";
+
+    cars.forEach(car => {
+        let carItem = document.createElement("div");
+        carItem.className.add("car-item");
+
+        carItem.innerText = 
+        `<img src =${car.image_url} alt =${car.make}>
+        <h3>${car.make} ${car.model}</h3>
+        <button onclick = showCarDetails(${car.id})>View Details</button>`;
+
+        gallery.appendChild(carItem);
+    })
+};
+
